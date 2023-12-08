8eaa0e3ef5bd1f295deaa79d71bb8237d754b1d9
test <test@test.com>
2023-12-09 01:31:25 +0530

    Added the Code directory with 3 exercises

---
+++ Code/ex1.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# STUDY DRILLS
+print("printing another line")
+print("printing one line")  # to print only one line (comment out other lines except this)
+# putting '#' before a line turns it into a single line comment (these are not executed)
+
